6a6cdca720b1c1ac2e6e45765f7ed9d2e3457404
1zemzem <1zemliakova@gmail.com>
2021-07-11 14:33:42 +0300

    LoginForm

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,39 +1,50 @@
-import React, { } from "react";
+import React from "react";
 
-export default class LoginForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {login: '', password: ''};
-  
-      this.onChangeLogin = this.onChangeLogin.bind(this);
-      this.onChangePassword = this.onChangePassword.bind(this);
-      this.onSubmit = this.onSubmit.bind(this);
-    }
-    
-    onSubmit(event){
-      alert(`${this.state.login}, добро пожаловать!`);
-      event.preventDefault();
-    }
-  
-    onChangePassword(event){
-      this.setState({password: event.target.value});
-    }
-    
-    onChangeLogin(event) {
-      this.setState({login: event.target.value});
-    }
-  
-    render() {
-      return (
-        <form onSubmit={this.onSubmit}>
-          <p><label> Логин: <input type="text" name="login" value={this.state.login}
-                           onChange={this.onChangeLogin}/></label></p>
-          <p><label> Пароль: <input type="password" name="password" value={this.state.password}
-                            onChange={this.onChangePassword}/></label></p>
-          <p><input type="submit" value="Submit" /></p>
-        </form>
-      );
-    }
-  }
-  
- // ReactDOM.render(<LoginForm />,  document.getElementById('root'));
+export default function LoginForm() {
+  const [login, setLogin] = useState(" ");
+  const [password, setPassword] = useState(" ");
+
+  const onSubmit = () => {
+    alert(`{login}, добро пожаловать!`);
+  };
+
+  const onChangePassword = () => {
+    setPassword({ password });
+  };
+
+  onChangeLogin = () => {
+    setLogin({ login });
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <p>
+        <label>
+          {" "}Логин:{" "}
+          <input
+            type="text"
+            name="login"
+            value={login}
+            onChange={onChangeLogin}
+          />
+        </label>
+      </p>
+      <p>
+        <label>
+          {" "}Пароль:{" "}
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChangePassword}
+          />
+        </label>
+      </p>
+      <p>
+        <input type="submit" value="Submit" />
+      </p>
+    </form>
+  );
+}
+
+// ReactDOM.render(<LoginForm />,  document.getElementById('root'));
